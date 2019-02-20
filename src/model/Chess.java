ea716db06105e45143d660b2322167295a2a5e11
Cooper <coopersun99@gmail.com>
2019-02-20 10:30:33 -0800

    Implemented chess class with getters and setters

--- src/model/Chess.java
+++ src/model/Chess.java
@@ -1,8 +1,35 @@
 package model;
 
 public class Chess {
-    private int size;
+    private static int size;
     private int x;
     private int y;
-    private boolean color;
+    private boolean color;   //true for white, false for black
+
+    public Chess(int x, int y, boolean color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        //size??
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean getColor() {
+        return color;
+    }
+
+    public void setColor(boolean color) {
+        this.color = color;
+    }
 }
